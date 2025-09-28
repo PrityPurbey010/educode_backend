bfaded05fc2160bd4d058f88a7531e9b84a3e2f8
Your NamePrityPurbey010 <you@example.compritypurbey25@gmail.com>
2025-09-28 08:32:41 +0530

    This is backend

---
+++ controller/admin_controller.js
@@ -0,0 +1,98 @@
+const Contact = require("../model/contact_model");
+const User  = require("../model/user_model");
+
+
+ const getAllUsers = async(req,res) => {
+    try {
+      const users = await User.find({}, {password:0});
+            
+      if(!users || users.length === 0){
+        return res.status(404).json({message:"no users found"});
+      }
+      return res.status(200).json(users);  
+    } catch (error) {
+        console.log(error);
+       
+    }
+};
+
+// single user logic
+const getUserById = async(req, res) => {
+  try {
+    const id = req.params.id;
+        
+  const data =  await User.findOne({ _id:id}, {password:0});
+  
+   return res.status(200).json(data);
+  } catch (error) {
+    console.log("error from userDetail by id api",error);
+    
+  }
+};
+
+ //  user update logic
+
+ const updateUserById = async (req,res) => {
+try {
+  const id = req.params.id;
+  const updateUserData = req.body;
+
+  const updatedData = await User.updateOne({_id:id}, {
+    $set: updateUserData,
+  }
+);
+ if (!updatedData.modifiedCount === 0) {
+  return res.status(200).json({ message: "No changes detected." });
+}
+   return res.status(201).json(updatedData);
+} catch (error) {
+
+   return res.status(500).json({ message: "Internal Server Error", error: error.message });
+  
+}
+ }
+  //  user delete logic
+
+const deleteUserById = async(req, res) => {
+  try {
+    const id = req.params.id;
+    await User.deleteOne({_id:id});
+   return res.status(200).json({message: "User Deleted successfully"});
+  } catch (error) {
+    console.log(error);
+   
+  };
+};
+//   getAllContacts logic
+ const getAllContact = async(req,res) => {
+    try {
+      const contacts = await Contact.find();
+            
+      if(!contacts ||contacts.length === 0){
+        return res.status(404).json({message:"no contacts found"});
+      }
+      return res.status(200).json(contacts);  
+    } catch (error) {
+        console.log(error);
+      
+    }
+};
+
+  //  contact delete logic
+
+
+
+const deleteContactById = async(req, res) => {
+  try {
+    const id = req.params.id;
+    await Contact.deleteOne({_id:id});
+   return res.status(200).json({message: "Contact Deleted successfully"});
+  } catch (error) {
+    console.log(error);
+   
+  };
+};
+
+
+
+module.exports ={ getAllUsers, getAllContact, deleteUserById ,getUserById , updateUserById ,deleteContactById};
---
+++ router/admin_router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router  =  express.Router();
+const adminController = require("../controller/admin_controller");
+const  authMiddleware =require("../middleware/auth_middleware");
+const adminMiddleware = require("../middleware/admin_middleware");
+
+
+router.route('/users').get(authMiddleware,adminMiddleware  , adminController.getAllUsers);
+router.route("/users/:id").get(authMiddleware,adminMiddleware, adminController.getUserById);
+router.route("/users/update/:id").patch(authMiddleware,adminMiddleware, adminController.updateUserById);
+router.route("/users/delete/:id").delete(authMiddleware, adminMiddleware,adminController.deleteUserById);
+router.route("/contacts").get(authMiddleware,adminMiddleware, adminController.getAllContact);
+router.route("/contacts/delete/:id").delete(authMiddleware,adminMiddleware, adminController.deleteContactById);
+
+
+
+module.exports = router;
